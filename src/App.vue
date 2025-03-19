ec107241105a2fd76f30ddcbb12b7a7f819e9822
Zyurkalov <zyurkalov@gmail.com>
2025-03-19 22:12:25 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/game">Game</RouterLink>
+        <RouterLink to="/honor-board">Score</RouterLink>
+
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+.wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  
+}
+
+
+nav {
+  width: 320px;
+  font-size: 12px;
+  margin-top: 2rem;
+
+  display: flex;
+  justify-content: space-between;
+}
+
+
+</style>
